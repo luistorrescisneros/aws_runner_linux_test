a157d27fa8f4e9e0628ad4455848b82f34b15a6a
luistorrescisneros <luis.torres.cisneros@sioux.eu>
2022-05-04 16:50:11 +0200

    Add modifications

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,7 @@ from app.hello import hello
 class TestHello(TestCase):
     
     def test_hello_true(self):
-        self.assertIn('AWS', hello() )
+        self.assertIn('AWS', hello())
 
     def test_hello_false(self):
         self.assertEqual('Hello Docker', hello())
